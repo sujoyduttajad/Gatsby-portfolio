f00ba3805817b43485863c94c9460a0a76ea5c0a
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2021-11-14 18:56:48 +0530

    Added mixins and gatsby logo

--- Portfolio/src/components/Navbar.js
+++ Portfolio/src/components/Navbar.js
@@ -1,18 +1,26 @@
 import React from "react"
 import scrollTo from "gatsby-plugin-smoothscroll"
+import logo from '../images/gatsby-icon.png'
+
 const Navbar = () => {
   return (
     <div className="section">
       <div className="container">
         <div className="navbar-wrapper">
+          
           <div
             role="button"
             onClick={() => scrollTo("#home")}
             className="name"
             tabIndex={0}
           >
-            My Portfolio <span><p>powered by </p> </span>
+            My Portfolio 
+            <span>
+              <p>powered by </p>
+              <img src={logo} alt="Gatsby" /> 
+            </span>
           </div>
+
           <div className="links-wrapper">
             <button onClick={() => scrollTo("#work")}>Projects</button>
             <button onClick={() => scrollTo("#employement")}>Work</button>
