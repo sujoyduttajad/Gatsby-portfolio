c52b676eb17f3cc6783e7d9b2b9af51be296452e
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2021-11-14 17:37:51 +0530

    Updated bio

--- Portfolio/src/yourdata.js
+++ Portfolio/src/yourdata.js
@@ -113,11 +113,11 @@ export default {
 
   // About Secton --------------
   aboutParaOne:
-    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+    "Hi there!",
   aboutParaTwo:
-    "It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    "I have 3.5 years of experience in Frontend development that comprises an impresive tech-stack of Next.js, React, Redux, Node, SASS, JavaScript, HTML5, CSS3, Bootstrap",
   aboutParaThree:
-    "It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    "I am a photographer also enjoy playing guitar in my free time. As a fun fact I'm learning cooking these days.",
   aboutImage:
     profilePic,
 
