7fe566cd20b3456b169d8c710b2a0d51816339b5
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2021-11-16 01:30:39 +0530

    Changed work to Project component

--- Portfolio/src/components/Projects.js
+++ Portfolio/src/components/Projects.js
@@ -4,7 +4,7 @@ import Fade from "react-reveal/Fade"
 
 import data from "../yourdata"
 
-const Work = () => {
+const Projects = () => {
   return (
     <div className="section" id="work">
       <div className="container">
@@ -32,4 +32,4 @@ const Work = () => {
   )
 }
 
-export default Work
+export default Projects
--- Portfolio/src/pages/index.js
+++ Portfolio/src/pages/index.js
@@ -4,7 +4,7 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 // Components
 import Header from "../components/Header"
-import Work from "../components/Work"
+import Projects from "../components/Projects"
 import About from "../components/about"
 import Skills from "../components/skills"
 import Promotion from "../components/Promotion"
@@ -14,7 +14,7 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Portfolio Template" />
     <Header></Header>
-    <Work></Work>
+    <Projects></Projects>
     <About></About>
     <Skills></Skills>
     <Promotion></Promotion>
