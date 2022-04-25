055f73492ce243a1d81f45e5e9d4ec1f23a6b598
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-04-25 15:46:39 +0530

    Update SkillCard.js

--- src/components/SkillCard.js
+++ src/components/SkillCard.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react"
 import { withStyles, makeStyles } from "@material-ui/core/styles"
-// import {Dialog, Button, DialogTitle, Typography, MuiDialogTitle} from '@material-ui/core';
 import Dialog from "@material-ui/core/Dialog"
 import Button from "@material-ui/core/Button"
 import DialogTitle from "@material-ui/core/DialogTitle"
