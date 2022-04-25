7c01f101e06d09d4a009c6e1a56c5f2eddb731b8
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-04-25 18:49:24 +0530

    Update Hamburger.js

--- src/components/atoms/Hamburger.js
+++ src/components/atoms/Hamburger.js
@@ -1,17 +1,28 @@
 import React from "react"
+import scrollTo from "gatsby-plugin-smoothscroll"
 
 const Hamburger = () => {
+
+
+ 
   return (
-    <div class="container">
-      <div class="row cf">
-        <div class="three col">
-          <div class="hamburger" id="hamburger-9">
-            <span class="line"></span>
-            <span class="line"></span>
-            <span class="line"></span>
+    <div className="nav-mobile">
+      <nav role="navigation">
+        <div className="menuToggle">
+          <input type="checkbox" />
+
+          <span></span>
+          <span></span>
+          <span></span>
+
+          <div className="nav-wrapper">
+            <button onClick={() => scrollTo("#work")}>Projects</button>
+            <button onClick={() => scrollTo("#employement")}>Work</button>
+            <button onClick={() => scrollTo("#about")}>About</button>
+            <button onClick={() => scrollTo("#contact")}>Contact</button>
           </div>
         </div>
-      </div>
+      </nav>
     </div>
   )
 }
