9bec418d40677729336cbd9ef8f63c305f3cf3db
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-04-25 15:46:46 +0530

    Update Card.js

--- src/components/atoms/Card.js
+++ src/components/atoms/Card.js
@@ -6,9 +6,7 @@ const Card = ({ heading, paragraph, imgUrl, projectLink }) => {
       className="card"
       style={{
         backgroundImage:
-          "linear-gradient(to bottom, rgba(245, 246, 252, 0), rgba(0, 0, 0, 0.2)),url(" +
-          imgUrl +
-          ")",
+          "url(" + imgUrl +")",
       }}
     >
       <div className="content">
