09e4c0e906cbed7573a2bfc8e9a3c1c138da2942
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-04-27 18:38:08 +0530

    Update Hamburger.js

--- src/components/atoms/Hamburger.js
+++ src/components/atoms/Hamburger.js
@@ -3,32 +3,35 @@ import scrollTo from "gatsby-plugin-smoothscroll"
 
 const Hamburger = () => {
   return (
-    <section className="nav-mobile">
-      <nav role="navigation">
-        <div className="menuToggle">
-          <input type="checkbox" />
+    <nav className="nav-mobile" role="navigation">
+      <div className="menuToggle">
+        <input type="checkbox" />
 
-          <span></span>
-          <span></span>
-          <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
 
-          <div className="nav-wrapper">
-            <button onClick={() => scrollTo("#work")}>
-              <p>Projects</p>
-            </button>
-            <button onClick={() => scrollTo("#employement")}>
-              <p>Work</p>
-            </button>
-            <button onClick={() => scrollTo("#about")}>
-              <p>About</p>
-            </button>
-            <button onClick={() => scrollTo("#contact")}>
-              <p>Contact</p>
-            </button>
+        <div className="nav-wrapper">
+          <button onClick={() => scrollTo("#work")}>
+            <p>Projects</p>
+          </button>
+          <button onClick={() => scrollTo("#employement")}>
+            <p>Work</p>
+          </button>
+          <button onClick={() => scrollTo("#about")}>
+            <p>About</p>
+          </button>
+          <button onClick={() => scrollTo("#contact")}>
+            <p>Contact</p>
+          </button>
+
+          <div className="ham-footer">
+            Build With <icon>❤</icon> by{" "}
+            <a href="https://github.com/sujoyduttajad">Sujoy Dutta</a>
           </div>
         </div>
-      </nav>
-    </section>
+      </div>
+    </nav>
   )
 }
 
