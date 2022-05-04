92a2697240288a46c527a3fd19f24742c3e58840
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-05-04 17:14:38 +0530

    Update WorkHistory.js

--- src/components/WorkHistory.js
+++ src/components/WorkHistory.js
@@ -3,25 +3,29 @@ import Fade from "react-reveal/Fade"
 import data from "../yourdata"
 // import { Chrono } from "react-chrono"
 import { makeStyles } from "@material-ui/core/styles"
-import Timeline from '@material-ui/lab/Timeline';
-import TimelineItem from '@material-ui/lab/TimelineItem';
-import TimelineSeparator from '@material-ui/lab/TimelineSeparator';
-import TimelineConnector from '@material-ui/lab/TimelineConnector';
-import TimelineContent from '@material-ui/lab/TimelineContent';
-import TimelineDot from '@material-ui/lab/TimelineDot';
-import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
+import Timeline from "@material-ui/lab/Timeline"
+import TimelineItem from "@material-ui/lab/TimelineItem"
+import TimelineSeparator from "@material-ui/lab/TimelineSeparator"
+import TimelineConnector from "@material-ui/lab/TimelineConnector"
+import TimelineContent from "@material-ui/lab/TimelineContent"
+import TimelineDot from "@material-ui/lab/TimelineDot"
+import TimelineOppositeContent from "@material-ui/lab/TimelineOppositeContent"
+import Paper from "@material-ui/core/Paper"
+import Typography from "@material-ui/core/Typography"
 
 const useStyles = makeStyles(theme => ({
   paper: {
     padding: "6px 16px",
-    
   },
   secondaryTail: {
     backgroundColor: theme.palette.secondary.main,
   },
   typography: {
     fontFamily: "'Lexend Deca', sans-serif",
+  },
+  workPeriod: {
+    fontFamily: "'Lexend Deca', sans-serif",
+    color: '#212121'
   }
 }))
 
@@ -50,23 +54,37 @@ const WorkHistory = () => {
             <Timeline align="alternate">
               {data.workHistory.map(work => (
                 <TimelineItem>
+                  <TimelineOppositeContent>
+                    <Typography
+                      color="textSecondary"
+                      variant="h6"
+                      className={classes.workPeriod}
+                    >
+                      {work.title}
+                    </Typography>
+                    <Typography
+                      color="textSecondary"
+                      variant="subtitle1"
+                      className={classes.typography}
+                    >
+                      {work.cardSubtitle}
+                    </Typography>
+                  </TimelineOppositeContent>
                   <TimelineSeparator>
                     <TimelineDot />
                     <TimelineConnector />
                   </TimelineSeparator>
                   <TimelineContent>
                     <Paper elevation={3} className={classes.paper}>
-                      <h2 className="work-company-name">
-                        {work.cardTitle}
-                      </h2>
+                      <h2 className="work-company-name">{work.cardTitle}</h2>
                       <ul className="work-company-list">
                         {work.cardDetailedText.map(detail => (
                           <li>
                             <Typography
-                                align="left"
-                                color="textSecondary"
-                                variant="body2"
-                                className={classes.typography}
+                              align="left"
+                              color="textSecondary"
+                              variant="body2"
+                              className={classes.typography}
                             >
                               {detail}
                             </Typography>
