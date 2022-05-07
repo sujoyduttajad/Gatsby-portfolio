40c11300f1d7fab75dd3ac66bed91e8274c68a51
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-05-07 10:54:43 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,32 +6,39 @@ import { format } from "date-fns"
 const Footer = () => {
   return (
     <div className="section" id="contact">
-      <div className="container">
-        <div className="footer-container">
-          <Fade bottom cascade>
-            <h1>Contact</h1>
-            <h2>{data.contactSubHeading}</h2>
-          </Fade>
-          <a className="email-link" href={`mailto:${data.contactEmail}`}>
-            {data.contactEmail}
-          </a>
-          <div className="social-icons">
-            {data.social.map((socialLink, index) => (
-              <a
-                key={index}
-                href={socialLink.url}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <img src={socialLink.img} alt="icons"></img>
-              </a>
-            ))}
+      <div className="footer-container">
+        <Fade bottom cascade>
+          <h1>Contact</h1>
+          <h2>{data.contactSubHeading}</h2>
+        </Fade>
+        <div className="footer-wrapper">
+          <div className="footer-links">
+            <a className="email-link" href={`mailto:${data.contactEmail}`}>
+              {data.contactEmail}
+            </a>
+            <div className="social-icons">
+              {data.social.map((socialLink, index) => (
+                <a
+                  key={index}
+                  href={socialLink.url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img src={socialLink.img} alt="icons"></img>
+                </a>
+              ))}
+            </div>
+            <span>
+              Build With <icon>❤</icon> by{" "}
+              <a href="https://github.com/sujoyduttajad">Sujoy Dutta</a>
+            </span>
+            <span>Copyright @{format(new Date(), "yyyy")}</span>
           </div>
-          <span>
-            Build With <icon>❤</icon> by{" "}
-            <a href="https://github.com/sujoyduttajad">Sujoy Dutta</a>
-          </span>
-          <span>Copyright @{format(new Date(), 'yyyy')}</span>
+          <div className="footer-form-container">
+            <form>
+                
+            </form>
+          </div>
         </div>
       </div>
     </div>
