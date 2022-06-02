8a14fa74c2d260cff730a3961053118dbb83b003
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-06-02 10:12:24 +0530

    Update SkillCard.js

--- src/components/SkillCard.js
+++ src/components/SkillCard.js
@@ -4,7 +4,8 @@ import Dialog from "@material-ui/core/Dialog"
 import Button from "@material-ui/core/Button"
 import DialogTitle from "@material-ui/core/DialogTitle"
 import MuiDialogContent from "@material-ui/core/DialogContent"
-import Tooltip from '@material-ui/core/Tooltip';
+import Tooltip from "@material-ui/core/Tooltip"
+import Fade from "react-reveal/Fade"
 
 const useStyles = makeStyles({
   button: {
@@ -34,11 +35,11 @@ const useStylesBootstrap = makeStyles(theme => ({
     color: theme.palette.common.black,
   },
   tooltip: {
-    backgroundColor: '#181818',
+    backgroundColor: "#181818",
     fontSize: "0.85em",
     fontFamily: "'Lexend Deca', sans-serif",
     fontWeight: 200,
-    borderRadius: "3px"
+    borderRadius: "3px",
   },
 }))
 function BootstrapTooltip(props) {
@@ -65,49 +66,51 @@ const SkillCard = ({ skill }) => {
   const classes = useStyles()
 
   return (
-    <div className="skill">
-      <img src={skill.img} alt="skills" />
-      <BootstrapTooltip title="Click to See More">
-        <Button
-          className={classes.button}
-          variant="outlined"
-          color="primary"
-          onClick={handleClickOpen}
-        >
-          {skill.header}
-        </Button>
-      </BootstrapTooltip>
-      <Dialog
-        onClose={handleClose}
-        aria-labelledby="customized-dialog-title"
-        open={open}
-      >
-        <DialogTitle
-          className={classes.dialogTitle}
-          id="customized-dialog-title"
+    <Fade bottom cascade>
+      <div className="skill">
+        <img src={skill.img} alt="skills" />
+        <BootstrapTooltip title="Click to See More">
+          <Button
+            className={classes.button}
+            variant="outlined"
+            color="primary"
+            onClick={handleClickOpen}
+          >
+            {skill.header}
+          </Button>
+        </BootstrapTooltip>
+        <Dialog
           onClose={handleClose}
-          disableTypography
-          style={{
-            background: `${skill.primaryColor}`,
-            color: `${skill.secondColor}`,
-          }}
+          aria-labelledby="customized-dialog-title"
+          open={open}
         >
-          <h3 className="span-header">{skill.header}</h3>
-          <ColorButton variant="outlined" onClick={handleClose}>
-            Close
-          </ColorButton>
-        </DialogTitle>
-        <DialogContent
-          dividers
-          style={{
-            background: `${skill.primaryColor}`,
-            color: `${skill.secondColor}`,
-          }}
-        >
-          <p className="skill-content">{skill.para}</p>
-        </DialogContent>
-      </Dialog>
-    </div>
+          <DialogTitle
+            className={classes.dialogTitle}
+            id="customized-dialog-title"
+            onClose={handleClose}
+            disableTypography
+            style={{
+              background: `${skill.primaryColor}`,
+              color: `${skill.secondColor}`,
+            }}
+          >
+            <h3 className="span-header">{skill.header}</h3>
+            <ColorButton variant="outlined" onClick={handleClose}>
+              Close
+            </ColorButton>
+          </DialogTitle>
+          <DialogContent
+            dividers
+            style={{
+              background: `${skill.primaryColor}`,
+              color: `${skill.secondColor}`,
+            }}
+          >
+            <p className="skill-content">{skill.para}</p>
+          </DialogContent>
+        </Dialog>
+      </div>
+    </Fade>
   )
 }
 
