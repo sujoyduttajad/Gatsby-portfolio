fbe895f50f8f95f6a503521fd7eb87218658d63d
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-06-02 10:12:26 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -75,65 +75,66 @@ const Footer = () => {
           <Fade bottom cascade>
             <h1>Contact</h1>
             <h2>{data.contactSubHeading}</h2>
-          </Fade>
-          <div className="footer-wrapper">
-            <div className="footer-links">
-              <a className="email-link" href={`mailto:${data.contactEmail}`}>
-                {data.contactEmail}
-              </a>
-              <div className="social-icons">
-                {data.social.map((socialLink, index) => (
-                  <a
-                    key={index}
-                    href={socialLink.url}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <img src={socialLink.img} alt="icons"></img>
-                  </a>
-                ))}
+
+            <div className="footer-wrapper">
+              <div className="footer-links">
+                <a className="email-link" href={`mailto:${data.contactEmail}`}>
+                  {data.contactEmail}
+                </a>
+                <div className="social-icons">
+                  {data.social.map((socialLink, index) => (
+                    <a
+                      key={index}
+                      href={socialLink.url}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <img src={socialLink.img} alt="icons"></img>
+                    </a>
+                  ))}
+                </div>
+                <span>
+                  Build With{" "}
+                  <span aria-label="heart-emoji" role="image">
+                    ❤️
+                  </span>{" "}
+                  by <a href="https://github.com/sujoyduttajad">Sujoy Dutta</a>
+                </span>
+                <span>Copyright @{format(new Date(), "yyyy")}</span>
               </div>
-              <span>
-                Build With{" "}
-                <span aria-label="heart-emoji" role="image">
-                  ❤️
-                </span>{" "}
-                by <a href="https://github.com/sujoyduttajad">Sujoy Dutta</a>
-              </span>
-              <span>Copyright @{format(new Date(), "yyyy")}</span>
+              <div className="footer-form-container">
+                <form>
+                  <CustomInput
+                    name="name"
+                    id="filled-required"
+                    label="Your Full Name"
+                    variant="outlined"
+                  />
+                  <CustomInput
+                    name="email"
+                    id="filled-required"
+                    label="Your Email"
+                    variant="outlined"
+                  />
+                  <CustomInput
+                    name="message"
+                    id="filled-required"
+                    label="Your Message"
+                    variant="outlined"
+                    multiline
+                    minRows={7}
+                  />
+                  <Button
+                    type="submit"
+                    className={classes.button}
+                    variant="contained"
+                  >
+                    Send Message
+                  </Button>
+                </form>
+              </div>
             </div>
-            <div className="footer-form-container">
-              <form>
-                <CustomInput
-                  name="name"
-                  id="filled-required"
-                  label="Your Full Name"
-                  variant="outlined"
-                />
-                <CustomInput
-                  name="email"
-                  id="filled-required"
-                  label="Your Email"
-                  variant="outlined"
-                />
-                <CustomInput
-                  name="message"
-                  id="filled-required"
-                  label="Your Message"
-                  variant="outlined"
-                  multiline
-                  minRows={7}
-                />
-                <Button
-                  type="submit"
-                  className={classes.button}
-                  variant="contained"
-                >
-                  Send Message
-                </Button>
-              </form>
-            </div>
-          </div>
+          </Fade>
         </div>
       </div>
     </div>
