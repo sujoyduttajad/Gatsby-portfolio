1c22e2b7fe168be9b78fe04bccb463ec0221c0d8
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-06-02 10:12:20 +0530

    Update WorkHistory.js

--- src/components/WorkHistory.js
+++ src/components/WorkHistory.js
@@ -4,24 +4,21 @@ import data from "../yourdata"
 import Timeline from "@material-ui/lab/Timeline"
 import TimelineCard from "./atoms/TimelineCard"
 
-
 const WorkHistory = () => {
-
-  
   return (
     <section className="section" id="employement">
       <div className="container">
         <div className="promotion-container">
           <Fade bottom cascade>
             <h1>{data.promotionHeading}</h1>
+            <div style={{ width: "80%", height: "50%" }}>
+              <Timeline align="alternate">
+                {data.workHistory.map((work, index) => (
+                  <TimelineCard key={index} work={work} />
+                ))}
+              </Timeline>
+            </div>
           </Fade>
-          <div style={{ width: "80%", height: "50%" }}>
-            <Timeline align="alternate">
-              {data.workHistory.map((work, index) => (
-                <TimelineCard key={index} work={work} />
-              ))}
-            </Timeline>
-          </div>
         </div>
       </div>
     </section>
