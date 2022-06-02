862fc9f06512811b79b409a64da141a879102812
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-06-02 10:13:25 +0530

    Update skills.js

--- src/components/skills.js
+++ src/components/skills.js
@@ -4,22 +4,17 @@ import SkillCard from "./SkillCard"
 import Fade from "react-reveal/Fade"
 
 const Skills = () => {
-  
-
   return (
     <div className="section">
       <div className="container">
         <div className="skills-container">
-        <Fade bottom cascade>
-          <h1>Skills</h1>
-          <div className="skills-grid">
-            {data.skills.map((skill, index) => (
-              <SkillCard
-                skill={skill}
-                key={index}
-              />
-            ))}
-          </div>
+          <Fade bottom cascade>
+            <h1>Skills</h1>
+            <div className="skills-grid">
+              {data.skills.map((skill, index) => (
+                <SkillCard skill={skill} key={index} />
+              ))}
+            </div>
           </Fade>
         </div>
       </div>
